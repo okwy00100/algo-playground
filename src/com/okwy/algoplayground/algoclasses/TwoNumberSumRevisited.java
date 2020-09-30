3f978533819049f4bd08cbced6868de55856cdf2
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2020-09-30 22:26:43 +0100

    Adding BST

---
+++ src/com/okwy/algoplayground/algoclasses/TwoNumberSumRevisited.java
@@ -0,0 +1,41 @@
+package com.okwy.algoplayground.algoclasses;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class TwoNumberSumRevisited {
+
+
+    public static void twoNumberSumRevisited(int[] numArray, int targetSum) {
+        //Time complexity - O(nlog(n))
+        //Space complexity - O(1)
+        int[][] sumPairs, indexPairs;
+        Arrays.sort(numArray);
+        int leftPointer = 0;
+        int rightPointer = numArray.length - 1;
+        while (leftPointer < rightPointer) {
+            int currentSum = numArray[leftPointer] + numArray[rightPointer];
+            if(currentSum == targetSum){
+
+            }else if(currentSum > targetSum){
+                rightPointer--;
+            }else{
+                leftPointer++;
+            }
+        }
+
+        System.out.println(Arrays.toString(new int[]{numArray[leftPointer], numArray[rightPointer]}));
+
+    }
+
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] numArray = new int[]{2, 3, 4, 1, 5, 6};
+        int sum = 7;
+
+        System.out.println(">> numArray.length >>" + numArray.length);
+        System.out.println(">> Target Sum >>" + sum);
+        twoNumberSumRevisited(numArray, sum);
+    }
+}
