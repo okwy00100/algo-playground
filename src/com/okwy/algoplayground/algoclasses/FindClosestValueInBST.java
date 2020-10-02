4068960ff39b2ed942e12eee969cb5218098304d
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2020-10-02 22:36:15 +0100

    Added DiskStacking Problem

--- src/com/okwy/algoplayground/algoclasses/FindClosestValueInBST.java
+++ src/com/okwy/algoplayground/algoclasses/FindClosestValueInBST.java
@@ -2,6 +2,16 @@ package com.okwy.algoplayground.algoclasses;
 
 public class FindClosestValueInBST {
 
+    public static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
