4679d0ef69184cd02adc145ba03651ebdf4e67e5
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2020-10-23 19:37:12 +0100

    Minimum Height BST

--- src/com/okwy/algoplayground/BinarySearchTrees/MinimumHeightBST.java
+++ src/com/okwy/algoplayground/BinarySearchTrees/MinimumHeightBST.java
@@ -1,5 +1,65 @@
 package com.okwy.algoplayground.BinarySearchTrees;
 
+import java.util.List;
+
 public class MinimumHeightBST {
+
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+
+        public void insert(int value) {
+            if (value < this.value) {
+                if (left == null) {
+                    left = new BST(value);
+                } else {
+                    left.insert(value);
+                }
+            } else {
+                if (right == null) {
+                    right = new BST(value);
+                } else {
+                    right.insert(value);
+                }
+            }
+        }
+    }
+
+    public static BST minHeightBst(List<Integer> array) {
+        // Write your code here.
+        return minHeightBSThelper(array, null, 0, array.size() -1);
+    }
+
+    public static BST minHeightBSThelper(List<Integer> array, BST bst, int startIndex, int endIndex){
+        if (startIndex > endIndex){
+            return null;
+        }
+        int midIndex = (startIndex + endIndex)/2;
+        int midValue = array.get(midIndex);
+        if(bst == null){
+            bst = new BST(midValue);
+        }else{
+            bst.insert(midValue);
+        }
+
+        minHeightBSThelper(array, bst, startIndex, midIndex - 1);
+        minHeightBSThelper(array, bst, midIndex + 1, endIndex);
+
+        return bst;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
 
 }
