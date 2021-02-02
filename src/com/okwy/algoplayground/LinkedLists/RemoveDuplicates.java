10c9d8a82d522c5ea88c07b8c475cd5362425745
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2021-02-02 22:57:34 +0100

    Added LinkedLists

---
+++ src/com/okwy/algoplayground/LinkedLists/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+package com.okwy.algoplayground.LinkedLists;
+
+public class RemoveDuplicates {
+
+    /**
+     * Description of the problem
+     * --------------------------
+     * Given the head of a singly linked list with sorted nodal values. Write a function that returns a new linkedlist with
+     * all duplicate value nodes. Note that the linked list should be modified in place and remain sorted.
+     *
+     */
+
+
+    public static void main(String[] args) {
+
+    }
+}
