8f41c2048e09c7cb6d4696864701f637f45563aa
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2021-04-06 14:09:30 +0100

    Added Tournament Winner Algo problem. Ongoing

---
+++ src/com/okwy/algoplayground/Arrays/TournamentWinner.java
@@ -0,0 +1,32 @@
+package com.okwy.algoplayground.Arrays;
+
+import java.util.ArrayList;
+
+public class TournamentWinner {
+
+    /**
+     * Description of the problem
+     * --------------------------
+     * There's a round robin tournament in which there can only be one winner and loser and no ties.
+     * If they wins, they get 3 points, 0 points if they lose. Given an array of pairs representing the teams that compete and
+     * an array containing the results of each competition, write a function that returns the winner of the tournament.
+     * The input arrays are "competitions" and "results" respectively. The competitions array has elements [hometeam, awayteam],
+     * where each team is a string of at most 30 characters representing the name of the team.
+     * The "results" array contains information about the winner of each corresponding competition in the "competitions" array.
+     * Specifically, "results[i]" denotes the winner of "competition[i]" where 1 means home won, and 0 means away won.
+     *
+     * It is guaranteed the tournament will always have at least two teams and also exactly one team will compete against all other
+     * teams exactly once meaning a winner of the tournament is guaranteed as well.
+     *
+     */
+
+    private static String tournamentWinner(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results){
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
