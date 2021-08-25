a502d6f40c1afa492c7113c83f825597aa12951e
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2021-08-25 23:14:41 +0100

    Added optimized solution for "First Non Repeating Character" Problem

--- src/com/okwy/algoplayground/Strings/FirstNonRepeatingCharacter.java
+++ src/com/okwy/algoplayground/Strings/FirstNonRepeatingCharacter.java
@@ -1,5 +1,8 @@
 package com.okwy.algoplayground.Strings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstNonRepeatingCharacter {
 
     /**
@@ -7,7 +10,7 @@ public class FirstNonRepeatingCharacter {
      * --------------------------
      * Give a function that takes a string of lowercase letters and returns the index of the
      * string's first non-repeating character
-     *
+     * <p>
      * It should return -1 if non was found
      */
 
@@ -15,14 +18,14 @@ public class FirstNonRepeatingCharacter {
     private static int firstNonRepeatingCharacterBrute(String string) {
         //  Time: O(n^2), Space: O(1)
         char[] stringArray = string.toCharArray();
-        for(int i = 0; i < stringArray.length; i++){
+        for (int i = 0; i < stringArray.length; i++) {
             boolean isRepeated = false;
-            for(int j = 0; j < stringArray.length; j++){
-                if(stringArray[i] == stringArray[j] && i!=j){
+            for (int j = 0; j < stringArray.length; j++) {
+                if (stringArray[i] == stringArray[j] && i != j) {
                     isRepeated = true;
                 }
             }
-            if(!isRepeated) return i;
+            if (!isRepeated) return i;
         }
         return -1;
     }
@@ -30,12 +33,24 @@ public class FirstNonRepeatingCharacter {
 
     private static int firstNonRepeatingCharacterOptimized(String string) {
 
+        Map<Character, Integer> charFrequency = new HashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            charFrequency.put(string.charAt(i), (charFrequency.get(string.charAt(i)) == null ? 0 : charFrequency.get(string.charAt(i))) + 1);
+        }
+
+        for (int i = 0; i < string.length(); i++) {
+            if (charFrequency.get(string.charAt(i)) == 1) {
+                return i;
+            }
+        }
         return -1;
     }
 
     public static void main(String[] args) {
 
         System.out.println(firstNonRepeatingCharacterBrute("abcdcaf"));
+
+        System.out.println(firstNonRepeatingCharacterOptimized("abcdcaf"));
 
     }
 }
