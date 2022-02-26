4456adf147d048656061cf2415cc547d2095b8aa
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2022-02-26 15:03:06 +0100

    Added simpler hourglass sum solution

--- src/com/okwy/algoplayground/Arrays/HourglassSum.java
+++ src/com/okwy/algoplayground/Arrays/HourglassSum.java
@@ -67,6 +67,28 @@ public class HourglassSum {
     }
 
 
+    private static int hourGlassSumSimple(List<List<Integer>> arr){
+
+        int max_sum = Integer.MIN_VALUE;
+
+
+        for(int i = 0; i < arr.size() - 2; i++){
+            for(int j = 0; j < arr.get(i).size() - 2; j++){
+
+                int current_sum = arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2)
+                        + arr.get(i+1).get(j+1)
+                        + arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);
+
+                max_sum = Math.max(current_sum, max_sum);
+
+            }
+        }
+
+
+        return max_sum;
+    }
+
+
     public static void main(String[] args) {
 
         List<List<Integer>> testList = new ArrayList<>();
@@ -79,5 +101,6 @@ public class HourglassSum {
 
         System.out.println("Maximum Hourglass sum>>>>>>>>>>>>>");
         System.out.println(hourglassSum(testList));
+        System.out.println(hourGlassSumSimple(testList));
     }
 }
