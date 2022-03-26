1a8b70bc6cfee32f7c49a1df2c0c45757283bbdb
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2022-03-26 11:31:38 +0100

    Interview prep still ongoing

---
+++ src/com/okwy/algoplayground/Interviews/Yelp/PrefixSearchTrie.java
@@ -0,0 +1,34 @@
+package com.okwy.algoplayground.Interviews.Yelp;
+
+public class PrefixSearchTrie {
+
+    /**
+     * Given a list of business_names (strings) and a searchTerm (string).
+     * Return a list of business_names that contains searchTerm as prefix in the business_names.
+     ***********************************************
+     * E.g.
+     * Example 1.
+     * Input:
+     * business_names[] = { "burger king", "McDonald's", "super duper burger's", "subway", "pizza hut"}
+     * searchTerm = "bur"
+     *
+     * Output:
+     * ["burger king", "super duper burger's"]
+     **********************************************
+     * Example 2
+     * Input:
+     * business_names[] = { "burger king", "McDonald's", "super duper burger's", "subway", "pizza hut"}
+     * searchTerm = "duper bur"
+     *
+     * Output:
+     * ["super duper burger's"]
+     *
+     * */
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
