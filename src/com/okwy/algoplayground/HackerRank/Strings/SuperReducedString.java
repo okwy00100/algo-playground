42a77657e087e195ce0ae403244a77b0055616a5
oknwachukwu <oknwachukwu@ecobank.com>
2022-04-04 23:08:39 +0100

    Added Hackerrank problem - Super reduced string

---
+++ src/com/okwy/algoplayground/HackerRank/Strings/SuperReducedString.java
@@ -0,0 +1,53 @@
+package com.okwy.algoplayground.HackerRank.Strings;
+
+import java.util.Stack;
+
+public class SuperReducedString {
+
+    /**
+     * Reduce a string of lowercase characters in range ascii[‘a’..’z’]by doing a series of operations.
+     * In each operation, select a pair of adjacent letters that match, and delete them.
+     *
+     * Delete as many characters as possible using this method and return the resulting string. If the final string
+     * is empty, return Empty String
+     *
+     * Example.
+     * s = aab
+     * aab shortens to b in one operation: remove the adjacent a characters.
+     *
+     * s = abba
+     * Remove the two 'b' characters leaving 'aa'. Remove the two 'a' characters to leave ''. Return 'Empty String'.
+     *
+     * */
+
+
+    private static String getReducedString(String s) {
+        char[] ch = s.toCharArray();
+        //Arrays.sort(ch);
+
+        Stack<Character> st = new Stack<Character>();
+        for (int i = 0; i < ch.length; i++) {
+            if (st.isEmpty()) {
+                st.push(ch[i]);
+
+            } else if (st.peek() == ch[i]) {
+                st.pop();
+            } else {
+                st.push(ch[i]);
+
+            }
+        }
+        String str = "";
+        while (!st.isEmpty()) {
+            str = st.pop() + str;
+            // str += st.pop();
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        String input = "aaabccddd";
+
+        System.out.println(getReducedString(input));
+    }
+}
