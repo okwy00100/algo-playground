ac1fa4ee236e152733ecff5254cf2a7eae321630
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2022-04-15 17:24:28 +0100

    Codility challenge - Missing Integer

---
+++ src/com/okwy/algoplayground/Arrays/MissingInteger.java
@@ -0,0 +1,44 @@
+package com.okwy.algoplayground.Arrays;
+
+/**
+ * @author okwy_nwachukwu
+ * created on 4/15/22 inside the package - com.okwy.algoplayground.Arrays
+ * project: algo-playground
+ */
+
+public class MissingInteger {
+
+    /**
+     *
+     * Write a function:
+     *
+     * class Solution { public int solution(int[] A); }
+     * that, given an array A of N integers, returns the smallest positive integer (greater than 0)
+     * that does not occur in A.
+     *
+     * For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+     *
+     * Given A = [1, 2, 3], the function should return 4.
+     *
+     * Given A = [−1, −3], the function should return 1.
+     * Assume that:
+     *
+     * N is an integer within the range [1..100,000]; each element of array A is an integer within the range
+     * [−1,000,000..1,000,000]. Complexity:
+     *
+     * expected worst-case time complexity is O(N); expected worst-case space complexity is O(N) (not counting the
+     * storage required for input arguments).
+     *
+     * */
+
+    private static int missingInteger(int[] A) {
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
